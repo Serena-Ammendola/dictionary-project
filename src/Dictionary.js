b7bdfa94ac71cf358df786b3b3dc46c09b88e13c
Serena-Ammendola <serena.ammendola95@gmail.com>
2024-05-29 22:52:12 +0100

    dictionary api added

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState(" ");
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+    let apiKey = "a471f3ac24t4fe3ab44b13f5c03c6o13";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
   function handeKeywordChange(event) {
     event.preventDefault();
